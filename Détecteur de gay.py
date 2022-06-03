17b1a4785fd1b4f3112241f02ebd8aeac55cfdcf
lapet3r123 <104019340+lapet3r123@users.noreply.github.com>
2022-06-03 17:18:18 +0200

    Add files via upload

---
+++ Détecteur de gay.py
@@ -0,0 +1,9 @@
+# import les librairies
+import random
+
+# ajoute les variable
+réponse = random.randint(0, 100)
+Username = input("entrez votre nom!")
+
+# écrie le tous
+print(Username, "\x1b[38;5;160m est gay a", réponse, "%")
